b514df7178c76b9acfcb7aacb5dd3cf2a215d160
ujjuengineer <uk3945351@gmail.com>
2026-06-25 18:21:35 +0530

    updated the website color grading

--- ngs_website/settings.py
+++ ngs_website/settings.py
@@ -83,3 +83,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 CONTACT_EMAIL = os.environ.get('CONTACT_EMAIL', 'info@narasimhaglobal.com')
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://narasimhaglobal.com",
+    "https://www.narasimhaglobal.com",
+]
